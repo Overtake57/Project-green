a57626e5312520c4907d696f8cc9656e58eae761
Overtake57 <agnes.yoann@gmail.com>
2023-02-01 21:57:28 +0100

    index done, design set
    Need to work on responsiv

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,18 @@
+// -------------- ABOUT ME ------------- //
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
+
+// -------------- ABOUT ME -------------- //
+
